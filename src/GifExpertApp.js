c41139a2039f369fb5e6b45691fb14d694b786be
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-11-20 17:27:00 -0600

    adding the first component

---
+++ src/GifExpertApp.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GifExpertApp = (props) => {
+
+    const pikey = 'AYhFpbImnSFFHIwxeoq82SNAeF5iDQXX'
+
+    return (
+        <>
+            <h2>Gif Expert App</h2>
+            <hr />
+        </>
+    )
+}
+
+GifExpertApp.propTypes = {
+
+}
+
+export default GifExpertApp
+
