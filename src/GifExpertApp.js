1889a31004c374bab56654639174b1f6eece8ce7
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-11-20 17:59:39 -0600

    creating a list

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
-const GifExpertApp = (props) => {
+const GifExpertApp = () => {
 
     const pikey = 'AYhFpbImnSFFHIwxeoq82SNAeF5iDQXX'
+
+    // const categories = ['One Punche Man','Naruto','Arcane']
+    const [categories, setCategories] = useState(['One Punche Man','Naruto','Arcane'])
+
+    const handleAdd = () => {
+        let newItem = `Game Of Thrones`;
+        
+        setCategories([...categories, newItem])
+    }
 
     return (
         <>
             <h2>Gif Expert App</h2>
             <hr />
+
+            <button onClick={handleAdd}>Add</button>
+
+            <ol>
+                { 
+                    categories.map( (category) => {
+                        return <li key={category}> { category } </li>
+                    })
+                }        
+            </ol>
+            
         </>
     )
 }
