791637d75b7bd4191b7236779a77330e38bc2ace
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-11-23 12:18:56 -0600

    hook created

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,24 +1,17 @@
-import React, {useState, useEffect} from 'react'
-import getGifs from '../helpers/getGifs'
+import React /*, {useState, useEffect}*/ from 'react'
+import useFetchGifs from '../hooks/useFetchGifs'
 import GifGridItem from './GifGridItem'
 
 const GifGrid = ({category}) => {
 
-    // const [count, setCount] = useState(1)
-
-    const [images, setImages] = useState([])
-
-    useEffect( () => {
-        getGifs( category )
-            // .then(imgs => setImages(imgs))
-            .then( setImages )
-    }, [category])
-
-    
+    const { data:images, loading } = useFetchGifs(category);
 
     return (
         <>
             <h3> {category} </h3>
+
+            { loading && <p>Loading at this moment bro...</p>}
+
             <div className='card-grid'>
                 {
                     images.map( img =>(
@@ -35,6 +28,27 @@ const GifGrid = ({category}) => {
 
 export default GifGrid;
 
+
+
+
+
+
+
+
+
+// import getGifs from '../helpers/getGifs'
+
+    // const [count, setCount] = useState(1)
+    // console.log(data, loading);
+
+/*const [images, setImages] = useState([]) */
+
+
+// useEffect( () => {
+    // getGifs( category )
+        // .then(imgs => setImages(imgs))
+        // .then( setImages )
+// }, [category])
 
 
 // images.map( ({id, title}) =>(
