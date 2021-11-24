f24f13ccfde743527a0f5a66289a9b3d002cb31b
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-11-24 11:32:08 -0600

    addign test should get the img equal to url and alt from the props

--- src/tests/componetns/GifGridItem.test.js
+++ src/tests/componetns/GifGridItem.test.js
@@ -7,13 +7,31 @@ describe('Test about Gif Grid Item component', ()=>{
 
     const title = 'a title';
     const url = 'https://localhost/algo.jpg' 
+    const wrapper = shallow( <GifGridItem title={title} url={url}/> )
+
 
     test('should show the component in the right way', () => {
 
-        const wrapper = shallow( <GifGridItem title={title} url={url}/> )
         expect( wrapper ).toMatchSnapshot();
 
     })
 
+    test("should have a <p> with the title in it",()=>{
+
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe(title)
+
+    })
+
+    test('should get the img equal to url and alt from the props', () => {
+        
+        const img = wrapper.find('img')
+        /*console.log(img.html());*/    //<img src="https://localhost/algo.jpg" alt="a title"/>
+        /*console.log(img.props().src); */ // `https://localhost/algo.jpg`
+        // console.log( img.prop('src')); same that the other one
+        expect( img.prop('src')).toBe(url);
+        expect( img.prop('alt')).toBe(title);
+    })
+    
 
 })
