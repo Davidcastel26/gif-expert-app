bf84b61a023d3f3d543a63ba89f88a67da6ba70e
David Castellanos <davcastellanoslarios@gmail.com>
2021-11-26 14:09:36 -0600

    should return an array of imgs and loading on false

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -5,9 +5,9 @@ import { renderHook } from '@testing-library/react-hooks'
 
 describe('testing in the hook useFetchGifs', () => {
     
-    test('should return the initial state', ()=>{
+    test('should return the initial state', async()=>{
 
-        const {result} = renderHook( () => useFetchGifs('Arcane') )
+        const {result, waitForNextUpdate} = renderHook( () => useFetchGifs('Arcane') )
         // console.log(resp);
 
         const {data, loading} = result.current;
@@ -15,8 +15,27 @@ describe('testing in the hook useFetchGifs', () => {
         // const {data, loading} = useFetchGifs( "category" )
         // console.log(data, loading); // [] , true
 
+        await waitForNextUpdate();
+
         expect( data ).toEqual([]);
         expect( loading ).toBe(true)
     })
 
+    test('should return an array of imgs and loading on false', async() => {
+        
+        //run the useFetch
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'Arcane' ));
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+
+        expect(data.length).toBe(10)
+        expect( loading ).toBe(false)
+
+    })
+    
+
 })
+
+//check the link to test a coustom hook
+//https://react-hooks-testing-library.com/#when-not-to-use-this-libr 
